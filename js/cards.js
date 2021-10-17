0509223dde7daef08dbf5af1e7585982bd41b0c5
Andrei Asipchuk <marsel20002008@yandex.ru>
2021-10-17 18:19:11 +0300

    Добавил деструктуризацию

--- js/cards.js
+++ js/cards.js
@@ -3,13 +3,13 @@ import { simularAds } from './data.js';
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const cardListFragment = document.createDocumentFragment();
 
-simularAds.forEach((card) => {
+simularAds.forEach(({author, offer}) => {
   const cardItem = cardTemplate.cloneNode(true);
-  cardItem.querySelector('.popup__title').textContent = card.offer.title;
-  cardItem.querySelector('.popup__text--address').textContent = card.offer.address;
-  cardItem.querySelector('.popup__text--price').textContent = `${card.offer.price}  ₽/ночь`;
+  cardItem.querySelector('.popup__title').textContent = offer.title;
+  cardItem.querySelector('.popup__text--address').textContent = offer.address;
+  cardItem.querySelector('.popup__text--price').textContent = `${offer.price}  ₽/ночь`;
   const popupType = cardItem.querySelector('.popup__type');
-  switch (card.offer.type) {
+  switch (offer.type) {
     case 'palace':
       popupType.textContent = 'Дворец';
       break;
@@ -26,27 +26,27 @@ simularAds.forEach((card) => {
       popupType.textContent = 'Отель';
       break;
   }
-  cardItem.querySelector('.popup__text--capacity').textContent = `${card.offer.rooms} комнаты для ${card.offer.guests} гостей`;
-  cardItem.querySelector('.popup__text--time').textContent = `Заезд после ${card.offer.checkin}, выезд до ${card.offer.checkout}`;
+  cardItem.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  cardItem.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
   const features = cardItem.querySelector('.popup__features');
   const featuresList = features.querySelectorAll('li');
   featuresList.forEach((feature) => {
-    const isAvailable = card.offer.features.some(
+    const isAvailable = offer.features.some(
       (item) => feature.classList.contains(`popup__feature--${item}`),
     );
     if (!isAvailable) {
       feature.remove();
     }
   });
-  cardItem.querySelector('.popup__description').textContent = card.offer.description;
+  cardItem.querySelector('.popup__description').textContent = offer.description;
   const popupPhotos = cardItem.querySelector('.popup__photos');
-  card.offer.photos.forEach((link) => {
+  offer.photos.forEach((link) => {
     const photo = popupPhotos.children[0].cloneNode(true);
     photo.src = link;
     popupPhotos.appendChild(photo);
   });
   popupPhotos.children[0].remove();
-  cardItem.querySelector('.popup__avatar').src = card.author.avatar;
+  cardItem.querySelector('.popup__avatar').src = author.avatar;
   cardListFragment.appendChild(cardItem);
 });
 
