1d6a9d85c865990398a095b322db377f5f710452
Andrei Asipchuk <marsel20002008@yandex.ru>
2021-10-18 09:08:17 +0300

    Исправил ошибки

--- js/form-control.js
+++ js/form-control.js
@@ -6,22 +6,22 @@ const filtersList = mapFilters.querySelectorAll('select, fieldset');
 const getInactiveForm = () => {
   adForm.classList.add('ad-form--disabled');
   mapFilters.classList.add('ad-form--disabled');
-  for (let i = 0; i < inputList.length; i++) {
-    inputList[i].setAttribute('disabled', 'disabled');
+  for (let index = 0; index < inputList.length; index++) {
+    inputList[index].setAttribute('disabled', 'disabled');
   }
-  for (let j = 0; j < filtersList.length; j++) {
-    filtersList[j].setAttribute('disabled', 'disabled');
+  for (let index = 0; index < filtersList.length; index++) {
+    filtersList[index].setAttribute('disabled', 'disabled');
   }
 };
 
 const getActiveForm = () => {
   adForm.classList.remove('ad-form--disabled');
   mapFilters.classList.remove('ad-form--disabled');
-  for (let i = 0; i < inputList.length; i++) {
-    inputList[i].removeAttribute('disabled', 'disabled');
+  for (let index = 0; index < inputList.length; index++) {
+    inputList[index].removeAttribute('disabled', 'disabled');
   }
-  for (let j = 0; j < filtersList.length; j++) {
-    filtersList[j].removeAttribute('disabled', 'disabled');
+  for (let index = 0; index < filtersList.length; index++) {
+    filtersList[index].removeAttribute('disabled', 'disabled');
   }
 };
 
