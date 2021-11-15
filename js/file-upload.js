83e27989f63a258435b4764304cff7e49f772b0b
Andrei Asipchuk <marsel20002008@yandex.ru>
2021-11-15 19:50:49 +0300

    Правки модуля file-upload.js

--- js/file-upload.js
+++ js/file-upload.js
@@ -5,27 +5,25 @@ const avatarPreview = adForm.querySelector('.ad-form-header__preview img');
 const photoChooser = adForm.querySelector('.ad-form__upload input[type="file"]');
 const photoPreviewBlock = adForm.querySelector('.ad-form__photo');
 
-avatarChooser.addEventListener('change', () => {
-  const file = avatarChooser.files[0];
+// колбэк для слушателя инпута
+function getUploadFile(inputTypeFile, preview) {
+  const file = inputTypeFile.files[0];
   const fileName = file.name.toLowerCase();
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
   if (matches) {
-    avatarPreview.src = URL.createObjectURL(file);
+    preview.src = URL.createObjectURL(file);
   }
+}
+
+avatarChooser.addEventListener('change', () => {
+  getUploadFile(avatarChooser, avatarPreview);
 });
 
 photoChooser.addEventListener('change', () => {
   photoPreviewBlock.innerHTML = '';
-  const photos = Array.from(photoChooser.files);
-  photos.forEach((item) => {
-    item.name.toLowerCase();
-    const matches = FILE_TYPES.some((it) => item.name.endsWith(it));
-    if (matches) {
-      const photoImage = document.createElement('img');
-      photoImage.src = URL.createObjectURL(item);
-      photoPreviewBlock.appendChild(photoImage);
-    }
-  });
+  const photoImage = document.createElement('img');
+  getUploadFile(photoChooser, photoImage);
+  photoPreviewBlock.appendChild(photoImage);
 });
 
 export { photoPreviewBlock, avatarPreview };
